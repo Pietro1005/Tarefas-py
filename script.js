70e908f0a71e9ab5083683586efa76bfebb4a561
Pietro1005 <pietrocorreali@colegiopaulodetarso.com.br>
2026-03-24 14:28:45 +0000

    sdsdsd

--- script.js
+++ script.js
@@ -3,5 +3,19 @@ const taskList = document.querySelector("#taskList")
 const taskInput = document.querySelector("#taskInput")
 
 taskBtn.addEventListener("click", function(){
-    alert("CLIQUEi !!!")
+    const li = document.createElement("li");
+    const taskinputValue = taskInput.value;
+        if (taskinputValue === ""){
+            return
+    }
+    const spam = document.createElement("spam");
+    spam.textContent = taskinputValue;
+    const button = document.createElement("button");
+    button.textContent = "Excluir";
+    }
+
+    li.appendChild(spam)
+    li.appendChild(button)
+
+    taskList.appendChild(li)
 })
